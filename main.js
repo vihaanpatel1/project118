1d51736cc552fb4ffe0be4e74216eaa91e19d50a
vihaanpatel1 <135666708+vihaanpatel1@users.noreply.github.com>
2024-02-07 16:45:08 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,37 @@
+function setup() {
+    canvas = createCanvas(280, 280);
+    canvas.center();
+    background("white")
+    canvas.mouseReleased(classifyCanvas);
+    synth = window.speechSynthesisCanvas;
+}
+
+function preload() {
+    classifier = ml5.imageClassifier('DoodleNet');
+}
+
+function draw(){
+    strokeWeight(13);
+    stroke(0);
+
+    if(mouseIsPressed) {
+        AudioListener(pmouseX, pmouseY, mouseX, mouseY);
+    }  
+}
+
+function classifyCanvas() {
+    classifier.classify(canvas, gotResult);
+}
+
+function gotResults(error, results) {
+    if ( error) {
+        console.log(error);
+    }
+    console.log(results);
+    document.getElementById('label').innerHTML = 'label:' + results[0].label;
+
+    document.getElementById('confidence').innerHTML = 'Confidence:' + Math.round(results[0].confidence * 100) + '%';
+
+    utterThis = new SpeechSynthesisUtterance(results[0].label);
+    synth.speak(utterThis);
+}
